8ebfd3ba337517107da1e2dc7fbd636c7da35716
Louisan Tchitoula <t.louisan@outlook.fr>
2024-09-15 22:25:53 +0200

    chore: default value for cancellation token on rating service interface

--- src/Movies.Application/RatingUseCases/Services/IRatingService.cs
+++ src/Movies.Application/RatingUseCases/Services/IRatingService.cs
@@ -2,6 +2,6 @@ namespace Movies.Application.RatingUseCases.Services;
 
 public interface IRatingService
 {
-    Task<bool> RateMovieAsync(int rating, Guid movieId, Guid userId, CancellationToken token);
-    Task<bool> DeleteRatingAsync(Guid movieId, Guid userId, CancellationToken token);
+    Task<bool> RateMovieAsync(int rating, Guid movieId, Guid userId, CancellationToken token = default);
+    Task<bool> DeleteRatingAsync(Guid movieId, Guid userId, CancellationToken token = default);
 }
